1eeebd88ae0c6147922ac4005e517e786806fe55
GITHub <shreesmani@yahoo.co.uk>
2021-11-02 03:42:11 +0530

    update

--- ABook/ABookAPP/urls.py
+++ ABook/ABookAPP/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index',)
+    path('', views.index, name='index',),
+    path('addaddress', views.addaddress, name='addaddress'),
+    path('viewaddress', views.viewaddress, name='viewaddress')
 ]
--- ABook/ABookAPP/views.py
+++ ABook/ABookAPP/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world");
+#    return HttpResponse("<h1>hello world</h1?");
+    return render(request, 'index.html', {'name': 'Sree Manikandan'})
+
+def addaddress(request):
+#    val1 = request.POST.get('num1')
+#    val2 = request.POST.get('num2')
+
+   # val = val1 + val2
+    #return render(request,'addaddress.html', {'result1': val1, 'result2': val2})
+    return render(request, 'addaddress.html')
+
+def viewaddress(request):
+    val1 = request.POST.get("slno")
+    val2 = request.POST.get("name")
+    val3 = request.POST.get("phonenumber")
+    val4 = request.POST.get("extension")
+    val5 = request.POST.get("email")
+    val6 = request.POST.get("address")
+
+    return render(request, 'viewaddress.html', {'slno':val1, 'name': val2, 'phonenumber': val3, 'extension': val4, 'email':val5, 'address' :val6})
